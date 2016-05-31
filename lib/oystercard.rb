e8a4ac3f807d77cacd671cee497ed9629fbcf5fd
Crescenzo D'Alterio <crescenzo.dalterio@gmail.com>
2016-05-31 15:31:57 +0100

    Challenge 04 complete - Adding balance attribute to Oystercard class

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,4 +2,12 @@
 
 
 class Oystercard
+
+  attr_reader :balance	
+
+  def initialize
+    @balance = 0
+  end	
+
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,12 @@
 #Oystercard test file.
+require 'oystercard'
+
+
 
 describe Oystercard do	
+  subject(:oystercard) {described_class.new}	
+  it "has a default balance of 0" do
+    expect(oystercard.balance).to eq 0
+  end
+
 end
