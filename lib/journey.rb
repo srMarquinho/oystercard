615233fc9bedeee5aed1df218231d5e237711243
riyapabari <riya.pabari@gmail.com>
2016-06-02 11:58:12 +0100

    Chapter 14: Adding journey class

--- lib/journey.rb
+++ lib/journey.rb
@@ -6,15 +6,22 @@ class Journey
 
   attr_accessor :entry_station, :exit_station
 
-  def initialize(entry_station = nil, exit_station = nil)
+  def initialize(entry_station = nil)
     @entry_station = entry_station
-    @exit_station = exit_station
   end
 
   def fare
     return 0 unless (entry_station || exit_station)
     return MAXIMUM_FARE unless (entry_station && exit_station)
     MINIMUM_FARE# + (entry_station.zone - exit_station.zone).abs
+  end
+
+  def finish(exit_station)
+    exit_station
+  end
+
+  def complete?
+    true
   end
 
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -2,31 +2,26 @@ require 'journey'
 
 describe Journey do
 
-  subject(:journey) {described_class.new}
-  subject(:complete_journey) {described_class.new(:entry_station, :exit_station)}
-  subject(:incomplete_journey) {described_class.new(:entry_station)}
+  subject(:journey) {described_class.new(:entry_station)}
+  subject(:journey) {described_class.new(:entry_station)}
 
-  describe 'Attributes' do
-    it 'Is initialized as an empty journey' do
-      expect(journey.entry_station || journey.exit_station).to be_nil
+  describe '#finish' do
+    it 'allows a journey to end' do
+      expect(journey.finish(:exit_station)).to eq :exit_station
     end
   end
 
-  describe '#fare' do
-    it 'returns 0 if user did NOT touch_in AND touch_out' do
-      expect(journey.fare).to eq 0
+  describe '#complete?' do
+    it 'returns true if a journey is complete' do
+      journey.finish(:exit_station)
+      expect(journey.complete?).to be true
     end
-
-    context 'user touched in and touched out' do
-      it 'returns the minimum fare' do
-        expect(complete_journey.fare).to eq Journey::MINIMUM_FARE
-      end
+    it 'returns false if a journey is incomplete' do
+      expect(journey.complete?). to be false
     end
-
-    context 'user forgot to touch_in or touch_out' do
-      it 'returns the penalty fare' do
-        expect(incomplete_journey.fare).to eq Journey::MAXIMUM_FARE
-      end
-    end
+    it 'returns '
   end
+
+
+
 end
